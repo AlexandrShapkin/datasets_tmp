e3a18c0f1c7babd02664ca50a8fa58014321ac7e
Alexandr Shapkin <130848312+AlexandrShapkin@users.noreply.github.com>
2026-06-30 15:04:31 +0700

    Use combined v12 dataset builder

--- scripts/run_open_set_builder.py
+++ scripts/run_open_set_builder.py
@@ -5,9 +5,9 @@ import json
 import traceback
 from pathlib import Path
 
-import build_cucumber_mixer_open_set_v11 as builder_v11
+import build_cucumber_mixer_open_set_v12 as builder_v12
 
-builder = builder_v11
+builder = builder_v12
 
 if __name__ == "__main__":
     try:
