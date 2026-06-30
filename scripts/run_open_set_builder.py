bf8fc7aafd70a29e80e350dc3566ad187ed98813
Alexandr Shapkin <130848312+AlexandrShapkin@users.noreply.github.com>
2026-06-30 16:57:07 +0700

    Use validated other-image selector v14

--- scripts/run_open_set_builder.py
+++ scripts/run_open_set_builder.py
@@ -5,9 +5,9 @@ import json
 import traceback
 from pathlib import Path
 
-import build_cucumber_mixer_open_set_v13 as builder_v13
+import build_cucumber_mixer_open_set_v14 as builder_v14
 
-builder = builder_v13
+builder = builder_v14
 
 if __name__ == "__main__":
     try:
