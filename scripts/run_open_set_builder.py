7a8f8ae02274c083360951365e6dd4e699651e15
Alexandr Shapkin <130848312+AlexandrShapkin@users.noreply.github.com>
2026-06-30 11:34:14 +0700

    Use efficient open-set downloader

--- scripts/run_open_set_builder.py
+++ scripts/run_open_set_builder.py
@@ -5,9 +5,9 @@ import json
 import traceback
 from pathlib import Path
 
-import build_cucumber_mixer_open_set_v4 as builder_v4
+import build_cucumber_mixer_open_set_v5 as builder_v5
 
-builder = builder_v4.builder
+builder = builder_v5.builder
 
 
 if __name__ == "__main__":
