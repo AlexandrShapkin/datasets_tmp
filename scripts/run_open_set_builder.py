f139f8605b2f9e10c67e9e24706a04869a7942f1
Alexandr Shapkin <130848312+AlexandrShapkin@users.noreply.github.com>
2026-06-30 14:02:48 +0700

    Use existing Wikimedia srcset thumbnails

--- scripts/run_open_set_builder.py
+++ scripts/run_open_set_builder.py
@@ -5,9 +5,9 @@ import json
 import traceback
 from pathlib import Path
 
-import build_cucumber_mixer_open_set_v10 as builder_v10
+import build_cucumber_mixer_open_set_v11 as builder_v11
 
-builder = builder_v10
+builder = builder_v11
 
 if __name__ == "__main__":
     try:
